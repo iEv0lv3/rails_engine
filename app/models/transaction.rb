fca5b4b514f56cba4939bcfe5f54ee71a0b41766
Jordan <jordan.wallace.williams@gmail.com>
2020-03-21 17:46:16 -0600

    Update Transaction model with additional enum option for result: failed

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   validates :credit_card_number, presence: true
   validates :credit_card_expiration_date, presence: true
   validates :result, presence: true
-  enum result: %w[success]
+  enum result: %w[success failed]
 
   belongs_to :invoice
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Transaction, type: :model do
     it { should validate_presence_of :credit_card_number }
     it { should validate_presence_of :credit_card_expiration_date }
     it { should validate_presence_of :result }
-    it { should define_enum_for(:result).with_values([:success]) }
+    it { should define_enum_for(:result).with_values([:success, :failed]) }
   end
 
   describe "relationships" do

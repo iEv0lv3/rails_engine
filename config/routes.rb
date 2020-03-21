c66d0b3670121e0950ba370c8ab0fc76b9b903bb
Jordan <jordan.wallace.williams@gmail.com>
2020-03-21 17:26:19 -0600

    Add initial routes for customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, except: [:new, :edit]
+      resources :customers, except: [:new, :edit]
     end
   end
 end

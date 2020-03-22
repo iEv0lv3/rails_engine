1b01c3b38bbcebdf91430ece91c26ea51cde1d0e
Jordan <jordan.wallace.williams@gmail.com>
2020-03-22 15:08:10 -0600

    Update Item factory to only include a single Merchant assosciation

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     name { Faker::Games::Dota.item }
     description { Faker::Hipster.sentence(word_count: 3) }
     unit_price { Faker::Commerce.price }
-    merchant { Faker::Company.name }
-    association :merchant, factory: :merchant
+    association :merchant
   end
 end

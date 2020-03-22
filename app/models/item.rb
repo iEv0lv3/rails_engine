af989e05679020f0951387a5e1008a800c6d2714
Jordan <jordan.wallace.williams@gmail.com>
2020-03-21 22:50:55 -0600

    Update maximum unit_price to 100,000

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  validates :unit_price, presence: true, numericality: { greater_than: 0, less_than: 10000 }
+  validates :unit_price, presence: true, numericality: { greater_than: 0, less_than: 100000 }
 
   belongs_to :merchant
   has_many :invoice_items

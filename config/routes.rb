3fc3bcf75aa1448b21e0ec3c608181237ea0ad5d
Jordan <jordan.wallace.williams@gmail.com>
2020-03-22 16:03:45 -0600

    Add namespaced routes for items and merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      namespace :items do
+        get '/:id/merchant', to: 'merchants#show'
+      end
+
+      namespace :merchants do
+        get '/:id/items', to: 'items#index'
+      end
+
       resources :merchants, except: [:new, :edit]
       resources :items, except: [:new, :edit]
     end

b2300e6178de096731d08e4557430f501b961897
Jordan <jordan.wallace.williams@gmail.com>
2020-03-22 15:08:32 -0600

    Update routes to include items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, except: [:new, :edit, :delete]
+      resources :merchants, except: [:new, :edit]
+      resources :items, except: [:new, :edit]
     end
   end
 end

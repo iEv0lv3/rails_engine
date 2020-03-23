22b71a021827375e1bf8127b2a563318a3751a12
Jordan <jordan.wallace.williams@gmail.com>
2020-03-22 20:01:45 -0600

    Update items search controller with all logic for single and multi finders

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,3 +1,23 @@
 class Api::V1::Items::SearchController < ApplicationController
+  def show
+    @items = search_params
+    render json: ItemSerializer.new(@items.first)
+  end
 
+  def index
+    @items = search_params.order(:name)
+    render json: ItemSerializer.new(@items)
+  end
+
+  private
+
+  def search_params
+    @items = Item.where(nil)
+    @items = Item.find_by_name(params[:name]) if params[:name].present?
+    @items = @items.find_by_description(params[:description]) if params[:description].present? 
+    @items = @items.find_by_unit_price(params[:unit_price]) if params[:unit_price].present? 
+    @items = @items.find_by_created(params[:created_at]) if params[:created_at].present?
+    @items = @items.find_by_updated(params[:updated_at]) if params[:updated_at].present?
+    @items
+  end
 end

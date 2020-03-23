065139f8bea95e0ddeb6e596bd6da34853ce2b2a
Jordan <jordan.wallace.williams@gmail.com>
2020-03-22 18:43:08 -0600

    Add model method tests for find_by_name, find_by_created, find_by_updated

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many :invoices }
   end
+
+  describe 'class methods' do
+    it 'find_by_name' do
+      merchant1 = create(:merchant, name: "Swag Shop")
+      merchant2 = create(:merchant, name: "The Swimmer's Friend")
+      merchant3 = create(:merchant, name: "Dota2 Shop")
+
+      expect(Merchant.find_by_name("sw")).to eq([merchant1, merchant2])
+    end
+
+    it 'find_by_created' do
+      merchant = create(:merchant, name: "Dota2 Shop")
+
+      expect(Merchant.find_by_created(merchant.created_at.to_s)).to eq([merchant])
+    end
+
+    it 'find_by_updated' do
+      merchant = create(:merchant, name: "Dota2 Shop")
+
+      expect(Merchant.find_by_updated(merchant.updated_at.to_s)).to eq([merchant])
+    end
+  end
 end

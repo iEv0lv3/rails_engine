1cf0dac1a71137f6322b448fa83d4419d868ffd0
Jordan <jordan.wallace.williams@gmail.com>
2020-03-22 20:01:19 -0600

    Add model methods for find_by_name, find_by_description, find_by_unit_price, find_by_created, find_by_update

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,40 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.find_by_name(search)
+    @items = where(
+      Item.arel_table[:name]
+      .lower
+      .matches("%#{search.downcase}%")
+    )
+  end
+
+  def self.find_by_description(search)
+    @items = where(
+      Item.arel_table[:description]
+      .lower
+      .matches("%#{search.downcase}%")
+    )
+  end
+
+  def self.find_by_unit_price(search)
+    @items = where(unit_price: search)
+  end
+
+  def self.find_by_created(created_at)
+    @items = where(
+      Item.arel_table[:created_at]
+      .to_s
+      .match(created_at)
+    )
+  end
+
+  def self.find_by_updated(updated_at)
+    @items = where(
+      Item.arel_table[:updated_at]
+      .to_s
+      .match(updated_at)
+    )
+  end
 end

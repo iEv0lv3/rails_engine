ad76c71c0e7b4b71d3219db243f19ee8d19f9979
Jordan <jordan.wallace.williams@gmail.com>
2020-03-22 23:53:34 -0600

    Update factory to include only the necessary associations

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invoice_item do
     quantity { rand(1..100) }
     unit_price { Faker::Commerce.price }
-    association :item, factory: :item
-    association :invoice, factory: :invoice
+    association :item
+    association :invoice
   end
 end
